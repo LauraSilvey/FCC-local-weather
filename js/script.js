df7b667272756d96fb6e59377f89f4737546e2ed
Laura Silvey <lsilvey@gmail.com>
2016-06-29 06:27:10 -0700

    added functionality to unit buttons and changed color scheme

--- js/script.js
+++ js/script.js
@@ -9,7 +9,13 @@ $(document).ready(function(){
       var url = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=adca06bc297c465fa3783238335eb81e&units=imperial";  
       $.getJSON(url, function(data){
         $("#location").html(data.name + ", " + data.sys.country);
-        $("i").html("  " + Math.floor(data.main.temp) + "&deg"); 
+        $("i").html("  " + Math.floor(data.main.temp) + "&deg");
+        $("#celsius").on("click", function(){
+          $("i").html("  " + Math.floor((data.main.temp - 32) * (5 / 9)) + "&deg");
+        });
+        $("#fahrenheit").on("click", function(){
+          $("i").html("  " + Math.floor(data.main.temp) + "&deg");
+        }); 
         var timeOfDay = "";
         if(data.dt > data.sys.sunrise && data.dt < data.sys.sunset){
           timeOfDay = "day-"
